7b1a9d5dab6ef2ea2911a41fb74ce20696fc9410
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2024-09-18 18:45:58 -0400

    implement user creation handler
    - adds the `create_user` function to handle POST requests to
    `/users/create`
    - adds the `check_email` and `check_username` functions to add checks

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -5,7 +5,7 @@ use std::{
 };
 
 use anyhow::Result;
-use axum::{routing::post, Router};
+use axum::{http::StatusCode, response::{IntoResponse, Response}, routing::post, Router};
 
 use sqlx::SqlitePool;
 use tokio::net::TcpListener;
@@ -14,7 +14,7 @@ use users::create_user;
 pub const PKG_NAME: &str = env!("CARGO_PKG_NAME");
 
 pub async fn start_server(pool: SqlitePool) -> Result<()> {
-    let app = Router::new().route("/", post(create_user)).with_state(pool);
+    let app = Router::new().route("/users/create", post(create_user)).with_state(pool);
     let tcp_listener = TcpListener::bind("0.0.0.0:3000").await?;
     axum::serve(tcp_listener, app).await?;
     Ok(())
@@ -30,4 +30,29 @@ pub async fn init_db(db_url: &str) -> Result<SqlitePool> {
     let pool = SqlitePool::connect_lazy(db_url)?;
     sqlx::migrate!("./migrations").run(&pool).await?;
     Ok(pool)
+}
+
+// Make our own error that wraps `anyhow::Error`.
+pub struct AppError(anyhow::Error);
+
+// Tell axum how to convert `AppError` into a response.
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self.0),
+        )
+            .into_response()
+    }
+}
+
+// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
+// `Result<_, AppError>`. That way you don't need to do that manually.
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
 }
--- api/src/users.rs
+++ api/src/users.rs
@@ -1,5 +1,72 @@
-use axum::{response::IntoResponse, Json};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use serde::{Deserialize, Serialize};
+use sqlx::SqlitePool;
+use anyhow::{anyhow, Result};
 
-pub async fn create_user(Json(data): Json<serde_json::Value>) -> impl IntoResponse {
-    "Hello, World!"
+use crate::AppError;
+
+#[derive(Serialize, Deserialize)]
+pub struct CreateUser {
+    pub email: String,
+    pub first_name: String,
+    pub last_name: Option<String>,
+    pub password: String,
+    pub username: String,
+}
+
+pub async fn create_user(
+    State(pool): State<SqlitePool>,
+    Json(user_data): Json<CreateUser>,
+) -> Result<impl IntoResponse, AppError> {
+    if !check_email(&user_data.email) {
+        return Ok((StatusCode::BAD_REQUEST, "Invalid email".into()));
+    }
+    if let Err(err) = check_username(&user_data.username) {
+        return Ok((StatusCode::BAD_REQUEST, err));
+    }
+    
+    if let Some(existing_user) = sqlx::query!(
+        "SELECT username, email FROM users where username = ? or email = ?",
+        user_data.username,
+        user_data.email
+    )
+    .fetch_optional(&pool)
+    .await
+    ?
+    {
+        if existing_user.username == user_data.username {
+            return Ok((StatusCode::CONFLICT, "Username already exists".into()));
+        } else {
+            return Ok((StatusCode::CONFLICT, "Email already in use".into()));
+        }
+    }
+    let hashed_password = password_auth::generate_hash(&user_data.password);
+
+    sqlx::query!(
+        "INSERT INTO users (username, email, password_hash, first_name, last_name) VALUES (?, ?, ?, ?, ?)",
+        user_data.username,
+        user_data.email,
+        hashed_password,
+        user_data.first_name,
+        user_data.last_name
+    ).execute(&pool).await?;
+
+    Ok((StatusCode::CREATED, "User created".into()))
+}
+
+pub fn check_email(email: &str) -> bool {
+    let re = regex::Regex::new(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$").expect("Should be a valid regex");
+    re.is_match(email)
+}
+
+pub fn check_username(username: &str) -> Result<(), Box<str>> {
+    if username.len() < 3 {
+        Err("Username must be at least 3 characters long".into())
+    } else if username.len() > 20 {
+        Err("Username must be at most 20 characters long".into())
+    } else if !username.chars().all(|c| c.is_alphanumeric() || c == '_') {
+        Err(r#"Username must only contain alphanumeric characters and _"#.into())
+    } else {
+        Ok(())
+    }
 }
