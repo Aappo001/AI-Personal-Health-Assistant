312e4435ee6d24cffa4c51bc457f75df19bdaa86
jordan.CD <jordancdiaz04@gmail.com>
2024-10-07 20:53:00 -0400

    setup react router

---
+++ client/src/components/PageNotFound.tsx
@@ -0,0 +1,7 @@
+export default function PageNotFound() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1 className="text-6xl">ERROR: Page Not Found</h1>
+    </div>
+  );
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import PageNotFound from "./components/PageNotFound.tsx";
 import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <PageNotFound />, //will load when an error or not found error occurs anywhere in the app
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
