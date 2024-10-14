8b55aa7c86b006742c04b4929b74ef838ff018b2
jordan.CD <jordancdiaz04@gmail.com>
2024-10-14 02:06:27 -0400

    style login

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { LoginBody } from "../types";
+import Background from "./Background";
 
 export default function Login() {
   const [login, setLogin] = useState<LoginBody>({
@@ -41,6 +42,61 @@ export default function Login() {
       setError(true);
     }
   };
+
+  return (
+    <Background color="black">
+      <div className="w-full h-full flex flex-col justify-center items-center">
+        <div className=" border-2 border-offwhite px-4 py-4 flex flex-col items-center justify-evenly gap-4 w-3/12 h-3/5 rounded:sm">
+          <p className={` text-offwhite text-6xl font-bebas`}>Login</p>
+          <form
+            className="flex flex-col justify-center items-center gap-9 w-5/6"
+            onSubmit={handleSubmit}
+          >
+            <div>
+              <input
+                type="text"
+                name="username"
+                value={login.username}
+                onChange={(e) => {
+                  setLogin({ ...login, username: e.target.value });
+                }}
+                placeholder="Username"
+                autoComplete="off"
+                required
+                className="w-full mt-8 pr-4 py-2 border-b-[1px] placeholder:text-surface75 focus:outline-none transition-colors duration-200 border-b-offwhite focus:border-b-lilac bg-main-black text-offwhite"
+              />
+              <input
+                type="password"
+                value={login.password}
+                name="password"
+                onChange={(e) => {
+                  setLogin({ ...login, password: e.target.value });
+                }}
+                placeholder="Password"
+                required
+                className="w-full mt-8 pr-4 py-2 border-b-[1px] placeholder:text-surface75 focus:outline-none transition-colors duration-200 border-b-offwhite focus:border-b-lilac bg-main-black text-offwhite"
+              />
+            </div>
+            <button
+              type="submit"
+              className={`px-5 py-3 border-2 rounded-full font-bold w-full transition-colors border-lilac text-lilac hover:bg-lilac hover:text-main-black`}
+            >
+              Submit
+            </button>
+          </form>
+          {responseMessage && (
+            <div
+              className={`mt-4 text-xl ${
+                error ? "text-red-500" : "text-green-500"
+              }`}
+            >
+              {responseMessage}
+            </div>
+          )}
+        </div>
+      </div>
+    </Background>
+  );
 
   return (
     <>
