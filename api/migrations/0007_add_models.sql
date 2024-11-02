b6403676ace6a8cbbc814b4018204ba165f2dc68
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2024-11-01 22:10:18 -0400

    added new ai models

---
+++ api/migrations/0007_add_models.sql
@@ -0,0 +1,8 @@
+INSERT INTO ai_models (name) VALUES ('mistralai/Mistral-Nemo-Instruct-2407');
+INSERT INTO ai_models (name) VALUES ('meta-llama/Llama-3.2-1B-Instruct');
+INSERT INTO ai_models (name) VALUES ('meta-llama/Llama-3.2-3B-Instruct');
+INSERT INTO ai_models (name) VALUES ('Qwen/Qwen2.5-72B-Instruct');
+INSERT INTO ai_models (name) VALUES ('google/gemma-1.1-2b-it');
+INSERT INTO ai_models (name) VALUES ('google/gemma-1.1-7b-it');
+INSERT INTO ai_models (name) VALUES ('google/gemma-2-9b-it');
+INSERT INTO ai_models (name) VALUES ('01-ai/Yi-1.5-34B-Chat');
