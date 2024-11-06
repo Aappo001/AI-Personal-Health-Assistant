eeeaf632b7fcadb8e722e87419af87cd9b6a18b6
jordan.CD <jordancdiaz04@gmail.com>
2024-11-06 01:09:26 -0500

    userIdMap Context

--- client/src/components/ChatSidebar.tsx
+++ client/src/components/ChatSidebar.tsx
@@ -1,8 +1,9 @@
 import { useContext, useEffect, useState } from "react";
 import RecentConversation from "./RecentConversation";
 import { useNavigate } from "react-router-dom";
-import { UserIdMapContext, WebsocketContext } from "./Chat";
+import { WebsocketContext } from "./Chat";
 import useFriendStore from "../store/hooks/useFriendStore";
+import { UserIdMap, useUserMapContext, useUserMapDispatchContext } from "./UserMapContext";
 
 export default function ChatSidebar({
   friends,
@@ -13,18 +14,21 @@ export default function ChatSidebar({
 }) {
   const navigate = useNavigate();
   const ws = useContext(WebsocketContext);
-  const userIdMap = useContext(UserIdMapContext);
+  const userMap = useUserMapContext();
+  const userMapDispatch = useUserMapDispatchContext();
   const friendStore = useFriendStore();
   const [activeConvo, setActiveConvo] = useState(-1);
   const [loading, setLoading] = useState(false);
 
-  const handleClick = (index: number) => {
-    if (index === activeConvo) {
+  const handleClick = (id: number) => {
+    if (id === activeConvo) {
       setActiveConvo(-1);
       navigate("/chat", { replace: true });
     } else {
-      setActiveConvo(index);
-      navigate(`/chat/messages/${friends[index]}`);
+      setActiveConvo(id);
+      const friend = userMap[id];
+      if (!friend) return;
+      navigate(`/chat/messages/${friend}`);
     }
   };
 
@@ -37,16 +41,24 @@ export default function ChatSidebar({
     ws.requestFriends();
   }, [ws, friendStore]);
 
+  useEffect(() => {
+    if (friendStore.length === 0) return;
+    const userMap: UserIdMap = {};
+    friendStore.forEach((friend) => {
+      userMap[friend.id] = friend.username;
+    });
+    userMapDispatch(userMap);
+  }, friendStore);
+
   return (
     <>
       <div className="absolute w-[23vw] h-full flex flex-col justify-center items-center gap-4">
         {loading && <p className="text-3xl text-offwhite">You have no friends!</p>}
+        {userMap && <p className="text-xl text-offwhite">{JSON.stringify(userMap)}</p>}
         {friendStore &&
-          friendStore.map((friend, index) => (
+          friendStore.map((friend) => (
             <RecentConversation
-              friend={friend.username}
-              index={index}
-              color={friend.color}
+              friend={friend}
               activeIndex={activeConvo}
               onClick={handleClick}
             />
--- client/src/components/RecentConversation.tsx
+++ client/src/components/RecentConversation.tsx
@@ -1,16 +1,14 @@
+import { Friend } from "../types";
+
 interface Props {
-  friend?: string;
+  friend: Friend;
   recentMessage?: string;
-  color?: string;
-  index: number;
   onClick: (index: number) => void;
   activeIndex: number;
 }
 export default function RecentConversation({
-  friend = "John Hancock",
+  friend,
   recentMessage = "No previous messages",
-  color = "bg-lilac",
-  index,
   onClick,
   activeIndex,
 }: Props) {
@@ -18,13 +16,13 @@ export default function RecentConversation({
     <>
       <div
         className={`flex gap-3 bg-main-grey ${
-          activeIndex === index && " bg-slate-700"
+          activeIndex === friend.id && " bg-slate-700"
         } p-4 rounded-lg w-10/12 cursor-pointer hover:scale-105`}
-        onClick={() => onClick(index)}
+        onClick={() => onClick(friend.id)}
       >
-        <span className={` w-12 h-12 ${color} rounded-full`}></span>
+        <span className={` w-12 h-12 ${friend.color} rounded-full`}></span>
         <div>
-          <p className="text-offwhite text-xl">{friend}</p>
+          <p className="text-offwhite text-xl">{friend.username}</p>
           <p className=" text-surface75">{recentMessage}</p>
         </div>
       </div>
