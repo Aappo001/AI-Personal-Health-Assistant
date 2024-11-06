Aappo001/AI-Personal-Health-Assistant

commit f216c8901c886d8d1e265905fb98fb1fc49d1b07

added file uploads on `POST` to `/api/upload` - files can now be uploaded to the server by providing their name and   base64 encoded data

api/src/upload.rs
@@ -1,0 +1,118 @@
+use std::path::PathBuf;
+
+use anyhow::anyhow;
+use axum::{
+    extract::State,
+    response::{IntoResponse, Response},
+};
+use base64::{engine::general_purpose, Engine};
+use chrono::NaiveDateTime;
+use mime::Mime;
+use reqwest::StatusCode;
+use serde::{Deserialize, Serialize};
+use sha1::{Digest, Sha1};
+use sqlx::SqlitePool;
+use tokio::{fs::File, io::AsyncWriteExt};
+
+use crate::error::{AppError, AppJson};
+
+/// A file to be uploaded to the server.
+#[derive(Deserialize)]
+pub struct FileUpload {
+    /// The name of the file to be stored.
+    file_name: Option<String>,
+    /// Base64 encoded file data.
+    file_data: String,
+}
+
+/// A file processed by the server.
+pub struct AppFile {
+    data: Vec<u8>,
+    mime: infer::Type,
+}
+
+impl AppFile {
+    pub fn from_base64(data: &str) -> Result<Self, AppError> {
+        let (head, encoded_data) = match data.split_once(',') {
+            Some((head, encoded_data)) => (Some(head), encoded_data),
+            // Assume that the data is just the base64 encoded data
+            None => (None, data),
+        };
+        let data = general_purpose::STANDARD.decode(encoded_data.as_bytes())?;
+        let mime = infer::get(&data).ok_or_else(|| {
+            AppError::UserError((
+                StatusCode::BAD_REQUEST,
+                "Unable to determine mime type".into(),
+            ))
+        })?;
+        if let Some(mut head) = head {
+            head = head
+                .split_once(';')
+                .ok_or_else(|| {
+                    AppError::UserError((StatusCode::BAD_REQUEST, "Invalid data".into()))
+                })?
+                .0;
+            head = head.strip_prefix("data:").unwrap_or(head);
+            if head != mime.mime_type() {
+                return Err(AppError::UserError((
+                    StatusCode::BAD_REQUEST,
+                    "Mime type mismatch".into(),
+                )));
+            }
+        }
+        Ok(Self { data, mime })
+    }
+}
+
+/// A file that has been uploaded to the server.
+#[derive(Serialize)]
+pub struct UploadedFile {
+    id: i64,
+    name: String,
+    path: String,
+    created_at: NaiveDateTime,
+    modified_at: NaiveDateTime,
+}
+
+pub async fn upload_file(
+    State(state): State<SqlitePool>,
+    AppJson(upload_data): AppJson<FileUpload>,
+) -> Result<Response, AppError> {
+    // Decode the base64 encoded data
+    let upload_file = AppFile::from_base64(&upload_data.file_data)?;
+
+    // Check if the file size is too large
+    if upload_file.data.len() > 10_000_000 {
+        return Err(AppError::UserError((
+            StatusCode::BAD_REQUEST,
+            "File size too large".into(),
+        )));
+    }
+
+    // Calculate the hash of the file to use as the filename
+    let hash = Sha1::digest(&upload_file.data).to_vec();
+
+    let file_name = format!(
+        "{}.{}",
+        hash.iter().map(|&x| x as char).collect::<String>(),
+        upload_file.mime.extension()
+    );
+    let path = PathBuf::from(format!(
+        "./uploads/{}",
+        file_name
+    ));
+
+    if !path.exists() {
+        let mut file = File::create(&path).await?;
+        file.write_all(&upload_file.data).await?;
+        let response_data = sqlx::query_as!(UploadedFile, "INSERT INTO files (name, path) VALUES (?, ?) RETURNING *", upload_data.file_name, file_name)
+            .fetch_one(&state)
+            .await?;
+        Ok((StatusCode::CREATED, AppJson(response_data)).into_response())
+    } else {
+        let response_data = sqlx::query_as!(UploadedFile, "SELECT * FROM files WHERE path = ?", file_name)
+            .fetch_one(&state)
+            .await?;
+        Ok((StatusCode::OK, AppJson(response_data)).into_response())
+    }
+}

api/Cargo.toml
@@ -55,3 +55,5 @@ console-subscriber = "0.4.1"
 rust-stemmers = "1.2.0"
 mime = "0.3.17"
 bytes = "1.8.0"
+sha1 = "0.10.6"
+infer = "0.16.0"

api/migrations/0009_create_images.sql
@@ -1,0 +1,12 @@
+CREATE TABLE files (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name TEXT NOT NULL,
+    path TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TRIGGER update_files_modified_at AFTER UPDATE ON files
+BEGIN
+	UPDATE files SET modified_at = CURRENT_TIMESTAMP WHERE id = NEW.id;
+END;

api/src/lib.rs
@@ -13,6 +13,8 @@ pub mod users;
 pub mod utils;
 /// Contains logic for processing user forms saving them to the database as statistics.
 pub mod forms;
+/// Contains logic for uploading files to the server.
+pub mod upload;
 use std::{
     fmt::Debug,
     net::SocketAddr,
@@ -48,6 +50,7 @@ use sqlx::{
 };
 use tokio::{net::TcpListener, sync::broadcast};
 use tracing::info;
+use upload::upload_file;
 use users::{
     authenticate_user, check_email, check_username, create_user, delete_user, get_user_by_id,
     get_user_by_username, get_user_from_token, update_user,
@@ -214,6 +217,8 @@ pub async fn start_server(pool: SqlitePool, args: &Args) -> Result<()> {
         .route("/forms/health/:id", put(update_health_form))
         // Used to show a user all the health forms they have submitted
         .route("/forms", get(get_forms))
+        // Used to upload files to the server
+        .route("/upload", post(upload_file))
         // .route("/chat/query_model/*model_name", get(query_model))
         .route("/ws", get(connect_conversation))
         .layer(cors);

commit a31a7835f57dba81e3619e52ceb8c9a979af9e1f

fixed up some bugs introduced in f216c89

.gitignore
@@ -1,5 +1,6 @@
 target/
 node_modules/
+uploads/
 .vscode/
 
 

api/migrations/0009_create_files.sql
@@ -1,7 +1,7 @@
 CREATE TABLE files (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     name TEXT NOT NULL,
-    path TEXT NOT NULL,
+    path TEXT UNIQUE NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );

api/src/upload.rs
@@ -1,13 +1,11 @@
-use std::path::PathBuf;
+use std::{fs::create_dir, io::ErrorKind, path::PathBuf};
 
-use anyhow::anyhow;
 use axum::{
     extract::State,
     response::{IntoResponse, Response},
 };
 use base64::{engine::general_purpose, Engine};
 use chrono::NaiveDateTime;
-use mime::Mime;
 use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
 use sha1::{Digest, Sha1};
@@ -90,29 +88,36 @@ pub async fn upload_file(
     }
 
     // Calculate the hash of the file to use as the filename
-    let hash = Sha1::digest(&upload_file.data).to_vec();
+    let hash = Sha1::digest(&upload_file.data);
 
-    let file_name = format!(
-        "{}.{}",
-        hash.iter().map(|&x| x as char).collect::<String>(),
-        upload_file.mime.extension()
-    );
-    let path = PathBuf::from(format!(
-        "./uploads/{}",
-        file_name
-    ));
+    let file_name = format!("{:x}.{}", hash, upload_file.mime.extension());
+    match create_dir("./uploads") {
+        Err(e) if e.kind() == ErrorKind::AlreadyExists => (),
+        Err(e) => return Err(e.into()),
+        _ => (),
+    }
+    let path = PathBuf::from(format!("./uploads/{}", file_name));
 
     if !path.exists() {
         let mut file = File::create(&path).await?;
         file.write_all(&upload_file.data).await?;
-        let response_data = sqlx::query_as!(UploadedFile, "INSERT INTO files (name, path) VALUES (?, ?) RETURNING *", upload_data.file_name, file_name)
-            .fetch_one(&state)
-            .await?;
+        let response_data = sqlx::query_as!(
+            UploadedFile,
+            "INSERT INTO files (name, path) VALUES (?, ?) ON CONFLICT DO NOTHING RETURNING *",
+            upload_data.file_name,
+            file_name
+        )
+        .fetch_one(&state)
+        .await?;
         Ok((StatusCode::CREATED, AppJson(response_data)).into_response())
     } else {
-        let response_data = sqlx::query_as!(UploadedFile, "SELECT * FROM files WHERE path = ?", file_name)
-            .fetch_one(&state)
-            .await?;
+        let response_data = sqlx::query_as!(
+            UploadedFile,
+            "SELECT * FROM files WHERE path = ?",
+            file_name
+        )
+        .fetch_one(&state)
+        .await?;
         Ok((StatusCode::OK, AppJson(response_data)).into_response())
     }
 }