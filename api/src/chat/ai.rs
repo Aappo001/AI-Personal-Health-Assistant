Aappo001/AI-Personal-Health-Assistant

commit eb5bdfa023e93eb69b54b4ffb0c9b1ee44592b0c

migrate from `serde_json` to `sonic_rs` - seems to be about twice as fast https://github.com/cloudwego/sonic-rs?tab=readme-ov-file#benchmark

api/Cargo.toml
@@ -43,7 +43,7 @@ regex = "1.10.6"
 reqwest = { version = "0.12.8", features = ["json", "stream"] }
 reqwest-streams = { version = "0.8.0", features = ["json"] }
 serde = "1.0.210"
-serde_json = "1.0.128"
+sonic-rs = "0.3.14"
 sqlx = { version = "0.8.2", features = ["runtime-tokio", "macros", "sqlite", "chrono", "json"] }
 tokio = { version = "1.40.0", features = ["full", "tracing"] }
 tokio-tungstenite = "0.24.0"
@@ -53,3 +53,5 @@ tracing-subscriber = { version = "0.3.18", features = ["env-filter"] }
 validator = { version = "0.18.1", features = ["derive"] }
 console-subscriber = "0.4.1"
 rust-stemmers = "1.2.0"
+mime = "0.3.17"
+bytes = "1.8.0"

api/src/chat/ai.rs
@@ -1,17 +1,17 @@
 use axum::{
     extract::State,
-    response::{IntoResponse, Json, Response},
+    response::{IntoResponse, Response},
 };
 use dotenvy::var;
 use futures::StreamExt;
 use reqwest::{header, StatusCode};
 use reqwest_streams::*;
 use serde::Serialize;
-use serde_json::json;
+use sonic_rs::{json, JsonValueTrait, JsonValueMutTrait};
 use sqlx::SqlitePool;
 use tracing::debug;
 
-use crate::{error::AppError, AppState};
+use crate::{error::{AppError, AppJson}, AppState};
 
 use super::{broadcast_event, SendMessage, SocketResponse};
 
@@ -61,7 +61,7 @@ pub async fn query_model(state: &AppState, message: &SendMessage) -> Result<Stri
         // Query the messages as a stream to save memory
         // This saves a ton on longer conversations
         let mut db_messages = sqlx::query!(
-            "SELECT message, user_id, ai_model_id, users.username FROM messages LEFT JOIN users ON messages.user_id = users.id WHERE conversation_id = ?",
+            "SELECT message, user_id, users.username FROM messages LEFT JOIN users ON messages.user_id = users.id WHERE conversation_id = ?",
             conversation_id
         )
         .fetch(&state.pool);
@@ -127,7 +127,7 @@ pub async fn query_model(state: &AppState, message: &SendMessage) -> Result<Stri
         .send()
         .await
         .map_err(AppError::from)?
-        .json_array_stream::<serde_json::Value>(2048);
+        .json_array_stream::<sonic_rs::Value>(2048);
 
     // The accumulated response from the AI model
     let mut res_content = String::new();
@@ -162,7 +162,7 @@ pub async fn query_model(state: &AppState, message: &SendMessage) -> Result<Stri
 pub async fn get_ai_models(State(pool): State<SqlitePool>) -> Result<Response, AppError> {
     Ok((
         StatusCode::OK,
-        Json(
+        AppJson(
             sqlx::query_as!(AiModel, "SELECT * FROM ai_models")
                 .fetch_all(&pool)
                 .await

api/src/chat/conversation.rs
@@ -4,7 +4,6 @@ use axum::{
     },
     http::StatusCode,
     response::{IntoResponse, Response},
-    Json,
 };
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
@@ -51,7 +50,7 @@ pub async fn get_user_conversations(
     )
     .fetch_all(&pool)
     .await?;
-    Ok((StatusCode::OK, Json(res)).into_response())
+    Ok((StatusCode::OK, AppJson(res)).into_response())
 }
 
 /// Create a conversation between the user and the AI from an initial message
@@ -65,7 +64,7 @@ pub async fn create_conversation_rest(
     // let title = &init_message.message[..cmp::min(init_message.message.len(), 32)];
     Ok((
         StatusCode::OK,
-        Json(create_conversation(&pool, &init_message, &user).await?),
+        AppJson(create_conversation(&pool, &init_message, &user).await?),
     )
         .into_response())
 }
@@ -165,7 +164,7 @@ pub async fn get_conversation(
         )
         .fetch_all(&pool)
         .await?;
-    Ok((StatusCode::OK, Json(res)).into_response())
+    Ok((StatusCode::OK, AppJson(res)).into_response())
 }
 
 /// A read receipt for a conversation

api/src/chat/websocket.rs
@@ -763,7 +763,7 @@ async fn handle_message(
 ) -> Result<(), AppError> {
     match msg {
         Message::Text(text) => {
-            let msg: SocketRequest = serde_json::from_str(&text)?;
+            let msg: SocketRequest = sonic_rs::from_str(&text)?;
             info!("Received {:?}", msg);
             match msg {
                 SocketRequest::SendMessage(mut send_message) => {
@@ -1080,7 +1080,7 @@ async fn send_message(
     if user.exp < chrono::Utc::now().timestamp() {
         return Ok(false);
     }
-    // *SAFETY* The `serde_json::to_string` function can safely be unwrapped because the `SocketResponse` enum
+    // *SAFETY* The `sonic_rs::to_string` function can safely be unwrapped because the `SocketResponse` enum
     // is serializable and will not panic
     // All responses should be serialized to JSON
     // and sent as Text
@@ -1088,7 +1088,7 @@ async fn send_message(
         SocketResponse::InternalCanceledGeneration => {}
         _ => {
             sender
-                .send(Message::Text(serde_json::to_string(&msg).unwrap()))
+                .send(Message::Text(sonic_rs::to_string(&msg).unwrap()))
                 .await?;
         }
     }

api/.cargo/config.toml
@@ -1,2 +1,2 @@
 [build]
-rustflags = ["--cfg", "tokio_unstable"]
+rustflags = ["--cfg", "tokio_unstable", "-C", "target-cpu=native"]

api/src/auth.rs
@@ -1,12 +1,11 @@
 use std::fmt::Display;
 
-use crate::error::ErrorResponse;
+use crate::error::{AppJson, ErrorResponse};
 use axum::{
     async_trait,
     extract::FromRequestParts,
     http::{header::AUTHORIZATION, request::Parts, StatusCode},
     response::{IntoResponse, Response},
-    Json,
 };
 use dotenvy_macro::dotenv;
 use jsonwebtoken::{decode, DecodingKey, Validation};
@@ -33,7 +32,7 @@ impl Display for JwtError {
 
 impl IntoResponse for JwtError {
     fn into_response(self) -> Response {
-        (StatusCode::UNAUTHORIZED, Json(ErrorResponse::from(self))).into_response()
+        (StatusCode::UNAUTHORIZED, AppJson(ErrorResponse::from(self))).into_response()
     }
 }
 

api/src/error.rs
@@ -2,13 +2,17 @@ use core::fmt;
 use std::fmt::{Display, Formatter};
 
 use axum::{
-    extract::rejection::JsonRejection,
-    http::StatusCode,
+    async_trait,
+    extract::{
+        rejection::{JsonRejection, MissingJsonContentType},
+        FromRequest, Request,
+    },
+    http::{HeaderMap, HeaderValue, StatusCode},
     response::{IntoResponse, Response},
-    Json,
 };
-use axum_macros::FromRequest;
-use serde::Serialize;
+use bytes::{BufMut, Bytes, BytesMut};
+use reqwest::header;
+use serde::{de::DeserializeOwned, Serialize};
 use tracing::{error, warn};
 use validator::Validate;
 
@@ -20,7 +24,7 @@ use crate::auth::JwtError;
 pub enum AppError {
     JsonRejection(JsonRejection),
     SqlxError(sqlx::Error),
-    SerdeError(serde_json::Error),
+    SerdeError(sonic_rs::Error),
     ValidationError(Vec<AppValidationError>),
     AuthError(anyhow::Error),
     UserError((StatusCode, Box<str>)),
@@ -44,7 +48,7 @@ impl From<AppError> for ErrorResponse {
     }
 }
 
-impl From<JwtError> for ErrorResponse{
+impl From<JwtError> for ErrorResponse {
     fn from(value: JwtError) -> Self {
         ErrorResponse {
             error_type: "AuthError".to_owned(),
@@ -59,8 +63,6 @@ impl From<JwtError> for ErrorResponse{
 // `axum::Json` responds with plain text if the input is invalid.
 /// A wrapper around `axum::Json` that provides a custom rejection to return JSON errors
 /// and allows us to intercept errors and provide a more detailed error message
-#[derive(FromRequest)]
-#[from_request(via(axum::Json), rejection(AppError))]
 pub struct AppJson<T>(pub T);
 
 /// A more descriptive error message for validation errors
@@ -114,7 +116,7 @@ impl IntoResponse for AppError {
         let (status, message) = match &self {
             AppError::JsonRejection(rejection) => (rejection.status(), rejection.body_text()),
             AppError::ValidationError(e) => {
-                (StatusCode::BAD_REQUEST, serde_json::to_string(&e).unwrap())
+                (StatusCode::BAD_REQUEST, sonic_rs::to_string(&e).unwrap())
             }
             AppError::SerdeError(e) => (StatusCode::BAD_REQUEST, e.to_string()),
             AppError::AuthError(e) => (StatusCode::UNAUTHORIZED, e.to_string()),
@@ -127,7 +129,7 @@ impl IntoResponse for AppError {
         // Return a JSON response with the error type and message.
         (
             status,
-            Json(ErrorResponse {
+            AppJson(ErrorResponse {
                 error_type: self.r#type(),
                 message,
             }),
@@ -157,7 +159,7 @@ impl Display for AppError {
         match self {
             AppError::JsonRejection(rejection) => write!(f, "{}", rejection.body_text()),
             AppError::SerdeError(e) => write!(f, "{}", e),
-            AppError::ValidationError(e) => write!(f, "{}", serde_json::to_string(&e).unwrap()),
+            AppError::ValidationError(e) => write!(f, "{}", sonic_rs::to_string(&e).unwrap()),
             AppError::AuthError(e) => write!(f, "{}", e),
             AppError::SqlxError(e) => write!(f, "{}", e),
             AppError::Generic(err) => write!(f, "{}", err),
@@ -180,13 +182,111 @@ where
         // We don't need to add `AuthError` or `ValidationError` because we will handle those
         // explicitly in our application.
         if err.downcast_ref::<JsonRejection>().is_some() {
-            return Self::JsonRejection(err.downcast().unwrap())
+            return Self::JsonRejection(err.downcast().unwrap());
         } else if err.downcast_ref::<sqlx::Error>().is_some() {
             return Self::SqlxError(err.downcast().unwrap());
-        } else if err.downcast_ref::<serde_json::Error>().is_some() {
+        } else if err.downcast_ref::<sonic_rs::Error>().is_some() {
             return Self::SerdeError(err.downcast().unwrap());
         } else {
             return Self::Generic(err);
+        }
+    }
+}
+
+impl<T> AppJson<T>
+where
+    T: DeserializeOwned,
+{
+    /// Construct a `Json<T>` from a byte slice. Most users should prefer to use the `FromRequest` impl
+    /// but special cases may require first extracting a `Request` into `Bytes` then optionally
+    /// constructing a `Json<T>`.
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self, AppError> {
+        let deserializer = &mut sonic_rs::Deserializer::from_slice(bytes);
+
+        let value = match serde::Deserialize::deserialize(deserializer) {
+            Ok(value) => value,
+            Err(err) => {
+                return Err(err.into());
+            }
+        };
+
+        Ok(AppJson(value))
+    }
+}
+
+fn json_content_type(headers: &HeaderMap) -> bool {
+    let content_type = if let Some(content_type) = headers.get(header::CONTENT_TYPE) {
+        content_type
+    } else {
+        return false;
+    };
+
+    let content_type = if let Ok(content_type) = content_type.to_str() {
+        content_type
+    } else {
+        return false;
+    };
+
+    let mime = if let Ok(mime) = content_type.parse::<mime::Mime>() {
+        mime
+    } else {
+        return false;
+    };
+
+    let is_json_content_type = mime.type_() == "application"
+        && (mime.subtype() == "json" || mime.suffix().map_or(false, |name| name == "json"));
+
+    is_json_content_type
+}
+
+#[async_trait]
+impl<T, S> FromRequest<S> for AppJson<T>
+where
+    T: DeserializeOwned,
+    S: Send + Sync,
+{
+    type Rejection = AppError;
+
+    async fn from_request(req: Request, state: &S) -> Result<Self, Self::Rejection> {
+        if json_content_type(req.headers()) {
+            let bytes = Bytes::from_request(req, state).await?;
+            Self::from_bytes(&bytes)
+        } else {
+            Err(AppError::JsonRejection(
+                JsonRejection::MissingJsonContentType(MissingJsonContentType::default()),
+            ))
+        }
+    }
+}
+
+// Use `AppJson` instead of `Json` to utilize `sonic_rs` for serialization
+// instead of `serde_json` which is slower.
+impl<T> IntoResponse for AppJson<T>
+where
+    T: Serialize,
+{
+    fn into_response(self) -> Response {
+        // Use a small initial capacity of 128 bytes like sonic_rs::to_vec
+        // https://docs.rs/sonic_rs/1.0.82/src/sonic_rs/ser.rs.html#2189
+        let mut buf = BytesMut::with_capacity(128).writer();
+        match sonic_rs::to_writer(&mut buf, &self.0) {
+            Ok(()) => (
+                [(
+                    header::CONTENT_TYPE,
+                    HeaderValue::from_static(mime::APPLICATION_JSON.as_ref()),
+                )],
+                buf.into_inner().freeze(),
+            )
+                .into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                [(
+                    header::CONTENT_TYPE,
+                    HeaderValue::from_static(mime::TEXT_PLAIN_UTF_8.as_ref()),
+                )],
+                err.to_string(),
+            )
+                .into_response(),
         }
     }
 }

api/src/forms.rs
@@ -1,7 +1,6 @@
 use axum::{
     extract::{Path, State},
     response::{IntoResponse, Response},
-    Json,
 };
 use chrono::NaiveDateTime;
 use macros::response;
@@ -50,7 +49,7 @@ pub async fn save_health_form(
     ).fetch_one(&state.pool).await?;
     Ok((
         StatusCode::CREATED,
-        Json(response!("Form successfully created", data)),
+        AppJson(response!("Form successfully created", data)),
     )
         .into_response())
 }
@@ -67,7 +66,7 @@ pub async fn get_health_form(
     )
     .fetch_one(&state.pool)
     .await?;
-    Ok((StatusCode::OK, Json(data)).into_response())
+    Ok((StatusCode::OK, AppJson(data)).into_response())
 }
 
 /// Get all the saved forms for the current user
@@ -82,7 +81,7 @@ pub async fn get_forms(
     )
     .fetch_all(&state.pool)
     .await?;
-    Ok((StatusCode::OK, Json(data)).into_response())
+    Ok((StatusCode::OK, AppJson(data)).into_response())
 }
 
 /// Get the most recent health form for the current user
@@ -124,7 +123,7 @@ pub async fn update_health_form(
 
     Ok((
         StatusCode::CREATED,
-        Json(response!("Form successfully created", data)),
+        AppJson(response!("Form successfully created", data)),
     )
         .into_response())
 }

commit ee5fa036a7bc511b407cdde838d070a8e3720e71

fixed message related bugs - fixed regression introduced in eb5bdfa that prevented receiving   messages from ai models. for some reason, attempting to serialize a   json array stream into `sonic_rs::Value` results in a `CodecError`, so   switch back to `serde_json::Value` for that stream - fixed regression introduced in b09812e, which resulted in an error when   attempting to retreive an ai generated `ChatMessage` when inserting it   into the database - single quotes in `SearchMessage` queries are now properly escaped to   prevent errors and sql injections

api/Cargo.toml
@@ -58,3 +58,4 @@ bytes = "1.8.0"
 sha2 = "0.10.8"
 infer = "0.16.0"
 tower = "0.5.1"
+serde_json = "1.0.132"

api/src/chat/ai.rs
@@ -2,13 +2,13 @@ use axum::{
     extract::State,
     response::{IntoResponse, Response},
 };
-use chrono::Timelike;
 use dotenvy::var;
 use futures::StreamExt;
 use reqwest::{header, StatusCode};
 use reqwest_streams::*;
 use serde::Serialize;
-use sonic_rs::{json, JsonValueMutTrait, JsonValueTrait};
+// use sonic_rs::{json, JsonValueTrait, JsonValueMutTrait};
+use serde_json::json;
 use sqlx::SqlitePool;
 use tracing::debug;
 
@@ -186,9 +186,12 @@ pub async fn query_model(
         )
         .json(&body)
         .send()
-        .await
-        .map_err(AppError::from)?
-        .json_array_stream::<sonic_rs::Value>(2048);
+        .await?
+        // Handle the response as a stream
+        // Using serde_json::Value instead of sonic_rs::Value because it breaks for some reason
+        // and gives a CodecError. I tried looking it up every where and even read through the
+        // source of both reqwest_streams and sonic_rs but I couldn't figure it out.
+        .json_array_stream::<serde_json::Value>(2048);
 
     // The accumulated response from the AI model
     let mut res_content = String::new();

api/src/chat/conversation.rs
@@ -7,7 +7,7 @@ use axum::{
 };
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
-use sqlx::{prelude::{FromRow, Type}, Decode, SqlitePool};
+use sqlx::{prelude::FromRow, SqlitePool};
 
 use crate::{
     auth::JwtAuth,

api/src/chat/search.rs
@@ -70,7 +70,7 @@ pub async fn search_message(
     search_message: &SearchMessage,
     sender: &broadcast::Sender<SocketResponse>,
 ) -> Result<(), AppError> {
-    let search_query = search_message.query.to_lowercase();
+    let search_query = search_message.query.replace("'", "\\'").to_lowercase();
     let search_query = search_query.trim();
     if search_query.is_empty() {
         return Ok(());

api/src/chat/websocket.rs
@@ -19,7 +19,7 @@ use base64::{engine::general_purpose, Engine};
 use chrono::NaiveDateTime;
 use futures::{stream::SplitSink, SinkExt, StreamExt};
 use serde::{Deserialize, Serialize};
-use sqlx::{Sqlite, SqlitePool};
+use sqlx::SqlitePool;
 use tokio::sync::broadcast;
 use tracing::{error, info, instrument, warn};
 
@@ -850,16 +850,16 @@ async fn handle_message(
                                     // This is done outside of the `query_model` function to
                                     // prevent the message from being lost if the user cancels
                                     // the AI generation while writing to the database
-                                    let message = sqlx::query_as::<Sqlite, ChatMessage>(
-                                        "INSERT INTO messages (conversation_id, message, stemmed_message, ai_model_id) VALUES (?, ?, ?, ?) RETURNING *"
+                                    let message = sqlx::query!(
+                                        "INSERT INTO messages (conversation_id, message, stemmed_message, ai_model_id) VALUES (?, ?, ?, ?) RETURNING id",
+                                        send_message.conversation_id,
+                                        ai_message,
+                                        stemmed_message,
+                                        ai_model_id
                                     )
-                                        .bind(send_message.conversation_id)
-                                        .bind(ai_message)
-                                        .bind(stemmed_message)
-                                        .bind(ai_model_id)
                                         .fetch_one(&state.pool)
-                                        .await?;
-                                    Ok(message)
+                                        .await?.id;
+                                    Ok(get_chat_message(&state.pool, message).await?)
                                 }
                                 _ = async {
                                     // Subscribe to the broadcast channel to listen for the
@@ -1121,10 +1121,7 @@ async fn send_message(
 }
 
 /// Utility function to easily extract a chat message from the database
-async fn get_chat_message(
-    pool: &SqlitePool,
-    message_id: i64,
-) -> Result<ChatMessage, AppError> {
+async fn get_chat_message(pool: &SqlitePool, message_id: i64) -> Result<ChatMessage, AppError> {
     Ok(sqlx::query_as!(
         ChatMessage,
         "SELECT messages.id, message, user_id, conversation_id, messages.created_at, ai_model_id, file_name, modified_at, files.path as file_path FROM messages LEFT JOIN files ON messages.id = files.id WHERE messages.id = ?",

api/src/lib.rs
@@ -21,7 +21,7 @@ use std::{
     ops::Deref,
     str::FromStr,
     sync::{atomic::AtomicI64, Arc},
-    time::Duration
+    time::Duration,
 };
 
 use anyhow::Result;
@@ -35,7 +35,11 @@ use forms::{get_forms, get_health_form, save_health_form, update_health_form};
 use reqwest::{header, Client};
 use tower::ServiceBuilder;
 use tower_http::{
-    compression::CompressionLayer, cors::{self, AllowOrigin, CorsLayer}, services::{ServeDir, ServeFile}, timeout::TimeoutLayer, trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer}, LatencyUnit, ServiceBuilderExt
+    cors::{self, AllowOrigin, CorsLayer},
+    services::{ServeDir, ServeFile},
+    timeout::TimeoutLayer,
+    trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer},
+    LatencyUnit, ServiceBuilderExt,
 };
 
 use chat::{
@@ -193,7 +197,7 @@ pub async fn start_server(pool: SqlitePool, args: &Args) -> Result<()> {
         .expose_headers([HeaderName::from_static("authorization")]);
 
     let sensitive_headers: Arc<[_]> = [header::AUTHORIZATION, header::COOKIE].into();
-       
+
     let middleware = ServiceBuilder::new()
         // Mark the `Authorization` and `Cookie` headers as sensitive so it doesn't show in logs
         .sensitive_request_headers(sensitive_headers.clone())
@@ -201,7 +205,11 @@ pub async fn start_server(pool: SqlitePool, args: &Args) -> Result<()> {
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(DefaultMakeSpan::new().include_headers(true))
-                .on_response(DefaultOnResponse::new().include_headers(true).latency_unit(LatencyUnit::Micros)),
+                .on_response(
+                    DefaultOnResponse::new()
+                        .include_headers(true)
+                        .latency_unit(LatencyUnit::Micros),
+                ),
         )
         .sensitive_response_headers(sensitive_headers)
         // Set a timeout