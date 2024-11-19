Aappo001/AI-Personal-Health-Assistant

commit 30aff6bb8e6b8280da5812344b39af600280f467

almost: RenameConversation awaiting bug fix, refactor of messageSlice to conversationSlice

client/src/store/hooks/useWebsocket.tsx
@@ -16,16 +16,17 @@ import {
   wsRequestFriendRequests,
   wsSendMessage,
   wsLeaveConversation,
+  wsRenameConversation,
 } from "../../utils/ws-utils";
 import { requestFriendsSchema } from "../../schemas";
 import useAppDispatch from "./useAppDispatch";
 import { addFriend, removeFriend, upgradeFriendStatus } from "../friendsSlice";
 import {
   deleteConversation,
-  initializeConversationId,
+  initializeConversation,
   pushMessage,
   pushStreamMessage,
-} from "../messageSlice";
+} from "../conversationSlice";
 import { Rootstate } from "../store";
 import { Friend } from "../../types";
 import { useSelector } from "react-redux";
@@ -112,15 +113,24 @@ export default function useWebsocketSetup() {
           break;
 
         case SocketResponse.Invite:
-          console.log(`Received Invite Message from user id ${data.inviter}`);
-          console.log(`Conversation Invite Data: ${JSON.stringify(data)}`);
-
-          dispatch(initializeConversationId(data.conversation_id));
+          console.log(`Received Invite Message from User ${data.inviter}`);
+          dispatch(initializeConversation({ id: data.conversation_id }));
           break;
 
         case SocketResponse.Conversation:
           console.log(`User present in conversation with id ${data.id}`);
-          dispatch(initializeConversationId(data.id));
+          console.log(JSON.stringify(data));
+          if (data.users) {
+            console.log(
+              `Request Conversation Data: Users = ${JSON.stringify(data.users)}, Title =  ${
+                data.title
+              }`
+            );
+            return;
+          }
+
+          dispatch(initializeConversation({ id: data.id, title: data.title }));
+          wsRequestConversation(socketRef.current, data.id);
           wsRequestMessages(socketRef.current, data.id);
           break;
 
@@ -212,6 +222,10 @@ export default function useWebsocketSetup() {
     leaveConversation: (conversationId: number) => {
       if (!socketRef.current) return;
       wsLeaveConversation(socketRef.current, conversationId);
+    },
+    renameConversation: (conversationId: number, name: string) => {
+      if (!socketRef.current) return;
+      wsRenameConversation(socketRef.current, conversationId, name);
     },
     loading,
   };

client/src/utils/ws-utils.ts
@@ -28,7 +28,7 @@ export const wsInviteUsersToConvo = (ws: WebSocket, ids: number[]) => {
 }
 
 export const wsRequestConversation = (ws: WebSocket, id: number) => {
-    console.log("Requesting singular conversation");
+    console.log(`Requesting Conversation ${id} data`);
     ws.send(JSON.stringify({
         type: "RequestConversation",
         conversation_id: id
@@ -71,6 +71,23 @@ export const wsLeaveConversation = (ws: WebSocket, conversationId: number) => {
     ws.send(JSON.stringify({
         type: "LeaveConversation",
         conversation_id: conversationId
+    }))
+}
+
+
+    // RenameConversation {
+    //     conversation_id: i64,
+    //     /// The new name of the conversation
+    //     /// If this is None, the frontend should fallback to listing the
+    //     /// usernames of the users in the conversation
+    //     name: Option<String>,
+    // },
+
+export const wsRenameConversation = (ws: WebSocket, conversationId: number, name: string) => {
+    ws.send(JSON.stringify({
+        type: "RenameConversation",
+        conversation_id: conversationId,
+        name: name
     }))
 }
 

client/public/check.svg
@@ -1,0 +1,1 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fffce1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>

client/public/edit.svg
@@ -1,0 +1,1 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fffce1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>

client/public/x.svg
@@ -1,0 +1,1 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fffce1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>

client/src/components/ChatMessagePage.tsx
@@ -1,28 +1,33 @@
 import { useParams } from "react-router-dom";
-import useMessageStore from "../store/hooks/useMessageStore";
 import { useUserMapContext, useUserMapDispatchContext } from "./UserMapContext";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { WebsocketContext } from "./Chat";
 import SpeechBubble from "./SpeechBubble";
 import useUserStore from "../store/hooks/useUserStore";
 import { getUserFromId } from "../utils/utils";
 import Toggle from "./Toggle";
+import useConversationStore from "../store/hooks/useConversationStore";
 
 export default function ChatMessagePage() {
   const user = useUserStore();
-  const messageStore = useMessageStore();
+  const conversationStore = useConversationStore();
   const userMap = useUserMapContext();
   const updateUserMap = useUserMapDispatchContext();
   const { handleSendMessage } = useContext(WebsocketContext);
   const [message, setMessage] = useState("");
+  const [newTitle, setNewTitle] = useState("");
+  const [editEnabled, setEditEnabled] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
   //@ts-expect-error setSelectedModel currently not used
   const [selectedModel, setSelectedModel] = useState(3);
   const [aiEnabled, setAiEnabled] = useState(false);
+  const { renameConversation } = useContext(WebsocketContext);
   let { id } = useParams();
   if (!id) {
     window.location.href = "/chat";
     return;
   }
+  const defaultTitle = `Conversation ${id}`;
 
   const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -34,11 +39,73 @@ export default function ChatMessagePage() {
     setMessage("");
   };
 
+  const handleRenameSubmit = () => {
+    renameConversation(parseInt(id), newTitle);
+    setEditEnabled(false);
+  };
+
+  useEffect(() => {
+    if (editEnabled && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [editEnabled]);
+
+  useEffect(() => {
+    setNewTitle(conversationStore[parseInt(id)]?.title ?? defaultTitle);
+    setEditEnabled(false);
+  }, [id]);
+
   return (
     <div className="flex flex-col justify-between items-center w-screen h-screen py-32">
-      <h1 className="text-6xl text-offwhite">Conversation {id}</h1>
+      <div className="flex gap-6">
+        {editEnabled ? (
+          <>
+            <input
+              type="text"
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
+              className="text-offwhite px-5 py-3 bg-transparent text-6xl focus:outline-none focus:outline-lilac "
+              ref={inputRef}
+            ></input>
+            <div className="flex flex-col justify-between items-center">
+              <img
+                src="/check.svg"
+                alt="Confirm"
+                height={30}
+                width={30}
+                className="cursor-pointer"
+                onClick={handleRenameSubmit}
+              />
+              <img
+                src="/x.svg"
+                alt="Cancel"
+                height={30}
+                width={30}
+                onClick={() => setEditEnabled(false)}
+                className="cursor-pointer"
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <h1 className="text-6xl text-offwhite ">
+              {conversationStore[parseInt(id)]?.title}
+            </h1>
+            <img
+              src="/edit.svg"
+              alt="Edit Conversation Title"
+              width={25}
+              height={25}
+              onClick={() => {
+                setEditEnabled((prev) => !prev);
+                setNewTitle(conversationStore[parseInt(id)]?.title || "");
+              }}
+            />
+          </>
+        )}
+      </div>
       <div className=" w-2/5 flex flex-col gap-4">
-        {messageStore[parseInt(id)]?.map((message, i) => {
+        {conversationStore[parseInt(id)]?.messages?.map((message, i) => {
           if (message.userId !== undefined && userMap[message.userId] === undefined) {
             console.log("UserMap userId is undefined");
 

client/src/components/ChatSidebar.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { WebsocketContext } from "./Chat";
 import useFriendStore from "../store/hooks/useFriendStore";
 import { UserIdMap, useUserMapContext, useUserMapDispatchContext } from "./UserMapContext";
-import useMessageStore from "../store/hooks/useMessageStore";
+import useConversationStore from "../store/hooks/useConversationStore";
 
 export default function ChatSidebar() {
   const navigate = useNavigate();
@@ -12,7 +12,7 @@ export default function ChatSidebar() {
   const userMap = useUserMapContext();
   const userMapDispatch = useUserMapDispatchContext();
   const friendStore = useFriendStore();
-  const messageStore = useMessageStore();
+  const conversationStore = useConversationStore();
   const [activeConvo, setActiveConvo] = useState(-1);
   const [loading, setLoading] = useState(false);
 
@@ -61,15 +61,16 @@ export default function ChatSidebar() {
         >
           <p className="text-xl text-offwhite p-4">Friends List</p>
         </div>
-        {Object.entries(messageStore).map(([conversationId, messages]) => (
+        {Object.entries(conversationStore).map(([conversationId, conversation]) => (
           <RecentConversation
             id={parseInt(conversationId)}
+            title={conversation?.title}
             activeIndex={activeConvo}
             onClick={handleClick}
             key={`convo-${conversationId}`}
             recentMessage={
-              messages
-                ? messages[messages.length - 1].content.slice(0, 16)
+              conversation?.messages?.length
+                ? conversation.messages[conversation.messages.length - 1].content.slice(0, 16)
                 : "No previous messages"
             }
           />

client/src/components/RecentConversation.tsx
@@ -4,12 +4,14 @@ import { useNavigate } from "react-router-dom";
 
 interface Props {
   id: number;
+  title: string | undefined;
   recentMessage?: string;
   onClick: (index: number) => void;
   activeIndex: number;
 }
 export default function RecentConversation({
   id,
+  title = `Conversation ${id}`,
   recentMessage = "No previous messages",
   onClick,
   activeIndex,
@@ -26,14 +28,14 @@ export default function RecentConversation({
           } p-4 rounded-lg rounded-r-none w-10/12 cursor-pointer `}
           onClick={() => onClick(id)}
         >
-          <span className={` w-12 h-12 bg-lilac rounded-full`}></span>
+          <span className={` w-12 h-12 bg-lilac rounded-full flex-shrink-0`}></span>
           <div>
-            <p className="text-offwhite text-xl">Conversation {id}</p>
+            <p className="text-offwhite text-xl">{title}</p>
             <p className=" text-surface75">{recentMessage}</p>
           </div>
         </div>
         <div
-          className={`bg-main-grey flex justify-center items-center ${
+          className={`bg-main-grey flex justify-center items-center flex-shrink-0 ${
             activeIndex === id && " bg-slate-700"
           } p-4 rounded-lg rounded-l-none cursor-pointer `}
         >

commit d4947cb3853e9bcc550a63063c9a87ac38cfaa69

convert all fields to `camelCase` and fix regression introduced in 30aff6b - changed all the fields in the websocket api to camelCase for the sake   of consistency - fixed a regression that prevented conversations from loading

api/src/chat/websocket.rs
@@ -98,6 +98,7 @@ pub enum SocketResponse {
     /// Stream data from the AI model
     StreamData(StreamMessage),
     /// Invite to a conversation
+    #[serde(rename_all = "camelCase")]
     Invite {
         /// The id of the conversation the user was invited to
         conversation_id: i64,
@@ -107,27 +108,25 @@ pub enum SocketResponse {
         invited_at: NaiveDateTime,
     },
     /// Event to inform the client that a user has left a conversation
-    LeaveEvent {
-        conversation_id: i64,
-        user_id: i64,
-    },
+    #[serde(rename_all = "camelCase")]
+    LeaveEvent { conversation_id: i64, user_id: i64 },
     /// Event to inform the client that a user renamed a conversation
+    #[serde(rename_all = "camelCase")]
     RenameEvent {
         conversation_id: i64,
         user_id: i64,
         name: Option<String>,
     },
     /// Friend request to be sent to the client
+    #[serde(rename_all = "camelCase")]
     FriendRequest {
         sender_id: i64,
         receiver_id: i64,
         created_at: chrono::NaiveDateTime,
         status: FriendRequestStatus,
     },
-    FriendData {
-        id: i64,
-        created_at: NaiveDateTime,
-    },
+    #[serde(rename_all = "camelCase")]
+    FriendData { id: i64, created_at: NaiveDateTime },
     /// Search results from a message query
     SearchMessage(ChatMessage),
     /// Error to inform the client
@@ -136,6 +135,7 @@ pub enum SocketResponse {
     /// in a conversation were read by a user
     ReadEvent(ReadEvent),
     /// AI generation was canceled in the conversation
+    #[serde(rename_all = "camelCase")]
     CanceledGeneration {
         conversation_id: i64,
         querier_id: i64,
@@ -171,10 +171,12 @@ enum SocketRequest {
     /// Edit a message in the conversation
     EditMessage(EditMessage),
     /// Deleted a message in the conversation
+    #[serde(rename_all = "camelCase")]
     DeleteMessage { message_id: i64 },
     /// Send, accept, reject, or revoke a friend request
     // Put all the friend request stuff in one enum variant
     // so its easier to handle on the frontend
+    #[serde(rename_all = "camelCase")]
     SendFriendRequest {
         /// The id of the user involved in the friend request
         /// This might be the sender or receiver depending on the action
@@ -185,6 +187,7 @@ enum SocketRequest {
         accept: bool,
     },
     /// Invite users to a conversation
+    #[serde(rename_all = "camelCase")]
     InviteUsers {
         /// The id of the conversation to invite the users to
         /// if this is None, a new conversation will be created
@@ -193,8 +196,10 @@ enum SocketRequest {
         invitees: Box<[i64]>,
     },
     /// Leave a conversation
+    #[serde(rename_all = "camelCase")]
     LeaveConversation { conversation_id: i64 },
     /// Rename a conversation
+    #[serde(rename_all = "camelCase")]
     RenameConversation {
         conversation_id: i64,
         /// The new name of the conversation
@@ -208,11 +213,13 @@ enum SocketRequest {
     /// Messages have been read in given conversation
     /// Does not provide user_id because the user is already authenticated
     /// Does not provide timestamp because the server will set it
+    #[serde(rename_all = "camelCase")]
     ReadMessage { conversation_id: i64 },
     /// Request the previous messages in the conversation
     /// Returns messages in order of most recent to least recent
     RequestMessages(RequestMessage),
     /// Request data on a conversation with the given id
+    #[serde(rename_all = "camelCase")]
     RequestConversation { conversation_id: i64 },
     /// Request a stream of conversations the user is in
     /// Returns conversations in order of last message sent

client/src/schemas.ts
@@ -53,17 +53,17 @@ export const conversationSchema = z.object({
 
 export const inviteSchema = z.object({
   type: z.literal("Invite"),
-  conversation_id: z.number(),
+  conversationId: z.number(),
   inviter: z.number(),
-  invited_at: z.string()
+  invitedAt: z.string()
 })
 
 // {"type":"FriendRequest","sender_id":1,"receiver_id":2,"created_at":"2024-10-26T04:00:40","status":"Pending"}
 export const friendRequestSchema = z.object({
   type: z.literal("FriendRequest"),
-  sender_id: z.number(),
+  senderId: z.number(),
   receiverId: z.string(),
-  created_at: z.string(),
+  createdAt: z.string(),
   status: z.string()
 })
 
@@ -85,5 +85,5 @@ export const requestConversationsSchema = z.object({
 export const requestFriendsSchema = z.object({
   type: z.literal("FriendData"),
   id: z.number(),
-  created_at: z.string()
+  createdAt: z.string()
 })

client/src/store/hooks/useWebsocket.tsx
@@ -81,8 +81,8 @@ export default function useWebsocketSetup() {
 
         case SocketResponse.FriendRequest:
           console.log("SocketResponse: FriendRequest");
-          const userIsSender = data.sender_id === userId;
-          let id = userIsSender ? data.receiver_id : data.sender_id;
+          const userIsSender = data.senderId === userId;
+          let id = userIsSender ? data.receiverId : data.senderId;
 
           if (data.status === "Accepted") {
             console.log("Friend Request accepted");
@@ -114,24 +114,14 @@ export default function useWebsocketSetup() {
 
         case SocketResponse.Invite:
           console.log(`Received Invite Message from User ${data.inviter}`);
-          dispatch(initializeConversation({ id: data.conversation_id }));
+          dispatch(initializeConversation({ id: data.conversationId }));
           break;
 
         case SocketResponse.Conversation:
           console.log(`User present in conversation with id ${data.id}`);
           console.log(JSON.stringify(data));
-          if (data.users) {
-            console.log(
-              `Request Conversation Data: Users = ${JSON.stringify(data.users)}, Title =  ${
-                data.title
-              }`
-            );
-            return;
-          }
 
           dispatch(initializeConversation({ id: data.id, title: data.title }));
-          wsRequestConversation(socketRef.current, data.id);
-          wsRequestMessages(socketRef.current, data.id);
           break;
 
         case SocketResponse.Error:
@@ -139,7 +129,7 @@ export default function useWebsocketSetup() {
           break;
 
         case SocketResponse.FriendData:
-          console.log(`Friends with user id ${data.id} at ${data.created_at}`);
+          console.log(`Friends with user id ${data.id} at ${data.createdAt}`);
           const privateUser = requestFriendsSchema.parse(data);
           getUserFromId(privateUser.id)
             .then((user) => {
@@ -154,9 +144,9 @@ export default function useWebsocketSetup() {
           break;
 
         case SocketResponse.LeaveEvent:
-          console.log(`User ${data.user_id} left conversation ${data.conversation_id}`);
-          if (userId === data.user_id) {
-            dispatch(deleteConversation(data.conversation_id));
+          console.log(`User ${data.userId} left conversation ${data.conversationId}`);
+          if (userId === data.userId) {
+            dispatch(deleteConversation(data.conversationId));
           }
           break;
 

client/src/utils/ws-utils.ts
@@ -2,7 +2,7 @@ export const wsSendFriendRequest = (ws: WebSocket, friendId: number, accept: boo
     ws.send(
         JSON.stringify({
           type: "SendFriendRequest",
-          other_user_id: friendId,
+          otherUserId: friendId,
           accept: accept,
         })
       );
@@ -13,7 +13,7 @@ export const wsRequestConversations = (ws: WebSocket) => {
     ws.send(JSON.stringify({
         type: "RequestConversations",
         // last_message_at: null,
-        message_num: 50
+        messageNum: 50
     }))
 }
 
@@ -21,7 +21,7 @@ export const wsInviteUsersToConvo = (ws: WebSocket, ids: number[]) => {
     console.log("Creating conversation..");
     ws.send(JSON.stringify({
         type: "InviteUsers",
-        conversation_id: null,
+        conversationId: null,
         invitees: ids
 
     }))
@@ -31,7 +31,7 @@ export const wsRequestConversation = (ws: WebSocket, id: number) => {
     console.log(`Requesting Conversation ${id} data`);
     ws.send(JSON.stringify({
         type: "RequestConversation",
-        conversation_id: id
+        conversationId: id
     }))
 }
 
@@ -70,7 +70,7 @@ export const wsSendMessage = (ws: WebSocket, message: string, conversationId?: n
 export const wsLeaveConversation = (ws: WebSocket, conversationId: number) => {
     ws.send(JSON.stringify({
         type: "LeaveConversation",
-        conversation_id: conversationId
+        conversationId: conversationId
     }))
 }
 
@@ -86,7 +86,7 @@ export const wsLeaveConversation = (ws: WebSocket, conversationId: number) => {
 export const wsRenameConversation = (ws: WebSocket, conversationId: number, name: string) => {
     ws.send(JSON.stringify({
         type: "RenameConversation",
-        conversation_id: conversationId,
+        conversationId: conversationId,
         name: name
     }))
 }