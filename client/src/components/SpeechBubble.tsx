c3b6a9448c64251c5d4338f449f7095e87887bc2
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2024-11-19 02:36:13 -0500

    render messages as markdown - all sent messages are rendered as markdown for better formatting - seeing as the ai models like to send responses with markdown   formatting in them, i think this change improves the overall   readability of messages

--- client/src/components/SpeechBubble.tsx
+++ client/src/components/SpeechBubble.tsx
@@ -1,3 +1,7 @@
+import DOMPurify from "dompurify";
+import { marked } from "marked";
+import { useEffect, useState } from "react";
+
 interface Props {
   message: string;
   from: string;
@@ -7,20 +11,24 @@ interface Props {
 export default function SpeechBubble({ message, from, isFromUser }: Props) {
   const bgColor = isFromUser ? "bg-lilac" : from === "AI" ? "bg-offwhite" : "bg-orangey";
   const spacing = isFromUser ? "self-end" : "self-start";
+  const [renderedMessage, setRenderedMessage] = useState("");
+  useEffect(() => {
+    const inner = async () => {
+      const rendered = await marked.parse(message);
+      setRenderedMessage(rendered);
+    }
+    inner()
+  }, [message]);
   return (
-    <div className="flex flex-col">
-      <p
-        className={`px-5 py-3 ${
-          message.length > 125 ? "rounded-3xl" : "rounded-full"
-        } text-xl ${bgColor} ${spacing}`}
-      >
-        {message}
-      </p>
+    <div className="speechBubble flex flex-col">
+      <div
+        className={`px-5 py-3 ${message.length > 125 ? "rounded-3xl" : "rounded-full"
+          } text-xl ${bgColor} ${spacing}`}
+        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(renderedMessage) }} />
       {!isFromUser && (
         <p
-          className={`${
-            from === "AI" ? "text-offwhite" : "text-orangey"
-          } font-semibold text-base ml-3`}
+          className={`${from === "AI" ? "text-offwhite" : "text-orangey"
+            } font-semibold text-base ml-3`}
         >
           {from}
         </p>
